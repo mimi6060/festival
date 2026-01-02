1a4c62147b20290ec6e064242968df869f9190ce
mimi6060 <mimi6060@users.noreply.github.com>
2026-01-02 15:32:17 +0100

    feat(utils): add shared utils barrel export

---
+++ festival/libs/shared/utils/src/index.ts
@@ -0,0 +1,17 @@
+// Festival Platform - Shared Utilities
+// Export all utility functions
+
+// Core utilities
+export * from './lib/date.utils';
+export * from './lib/string.utils';
+export * from './lib/validation.utils';
+export * from './lib/format.utils';
+export * from './lib/crypto.utils';
+
+// Extended utilities
+export * from './lib/currency.utils';
+export * from './lib/pagination.utils';
+export * from './lib/qrcode.utils';
+export * from './lib/geo.utils';
+export * from './lib/file.utils';
+export * from './lib/phone.utils';
